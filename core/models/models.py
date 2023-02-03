ce634c6209f1d1446189921eaefb7de2c2eb84ee
Kofi Boateng <kdboat2@gmail.com>
2023-02-02 18:08:40 -0600

    Began work on moving average strategy, and mean reversion (by mistake). Will decide on whether to use multiple moving averages for our trading strategy for the moving average. Mean Reversion is almost done.

--- core/models/models.py
+++ core/models/models.py
@@ -26,11 +26,17 @@ class Portfolio:
     current_available_funds:float
     total_funds:float
     holdings:dict
+    max_exposure_allowed:float
+    stop_loss:float
+    limit_order:float
 
-    def __init__(self,current_funds:float,total_funds:float,holdings:dict):
+    def __init__(self,current_funds:float,total_funds:float,holdings:dict,max_exposure_allowed:float,stop_loss_percent:float,limit_order_percent):
         self.current_available_funds = current_funds
         self.total_funds = total_funds
         self.holdings = holdings
+        self.max_exposure_allowed
+        self.stop_loss = 1 - stop_loss_percent
+        self.limit_order = 1 + limit_order_percent
 
     def get_current_funds(self) -> float:
         return self.current_available_funds
@@ -39,4 +45,13 @@ class Portfolio:
         return self.total_funds     
 
     def get_holdings(self) -> dict:
-        return self.holdings         
+        return self.holdings       
+
+    def get_max_exposure_allowed(self) -> float:
+        return self.max_exposure_allowed      
+
+    def get_stop_loss(self)-> float:
+        return self.stop_loss
+
+    def get_limit_order(self) -> float:
+        return self.limit_order    
--- core/strategy.py
+++ core/strategy.py
@@ -1,7 +1,8 @@
-from analyzer import MarketAnalyzer
 from forecaster import Forecaster
 from models.models import Portfolio,PriceHistory
 from typing import Set
+from concurrent.futures import ThreadPoolExecutor
+from math import floor
 
 class Strategy:
     
@@ -14,15 +15,36 @@ class Strategy:
     def execute_bollinger_band_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory]):
         pass
 
-    def execute_mean_reversion_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory]):
-        pass    
+    def execute_mean_reversion_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory],type:str,window:float):
+        numOfStocks = len(potential_stocks)
+        executor:ThreadPoolExecutor = ThreadPoolExecutor(max_workers=numOfStocks)
 
-    def execute_momentum_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory]):
-        pass
+        for ticker in potential_stocks:
+            executor.submit(self.__mean_reversion_task(ticker,portfolio,type,window))
+        executor.shutdown(wait=True)
     
     # WORK ON THIS FIRST
-    def execute_moving_average_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory], type:str):
+    def execute_moving_average_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory], type:str,window:float):
+        numOfStocks = len(potential_stocks)
+        executor:ThreadPoolExecutor = ThreadPoolExecutor(max_workers=numOfStocks)
 
+        for ticker in potential_stocks:
+            executor.submit(self.__moving_average_task(ticker,portfolio,type,window))
+        executor.shutdown(wait=True)
+
+       
+    def execute_pairs_trading_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory]):
+        pass
+
+    def execute_scalping_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory]):
+        pass
+
+    def __moving_average_task(self,ticker:PriceHistory,portfolio:Portfolio,type:str, window:float):
+        pass
+
+
+    def __mean_reversion_task(self,ticker:PriceHistory,portfolio:Portfolio,type:str, window:float):
+        import pandas as pd
         # 1. GET PORTFOLIO INFORMATION
         # 2. CALCULATE MOVING AVERAGE OF EACH POTENTIAL STOCK
         # 3. PERFORM MA STRATEGY TO SEE IF A BUY OR SELL IS BEING SIGNALED
@@ -32,23 +54,117 @@ class Strategy:
 
         # Get the portfolio's total funds, current funds and the holdings
         curr_funds,total_funds,holdings = portfolio.get_current_funds(),portfolio.get_total_funds(),portfolio.get_holdings()
-        for ticker in potential_stocks:
-            print(f'[STATE]:Currently on ticker:{ticker}')
-            # holdings is a dict where key = some ticker and value = another dictionary
-            # The value dictionary will hold the total invested amount and total number of
-            # shares. ---> {ticker:'GOOG',info:{shares:55,amount:$25,000}}
-            if ticker in holdings:
-                info:dict = holdings.get(ticker)
-                shares,amount = info.get("shares"),info.get("amount")
-                port_perc:float = (amount/total_funds)*100
+        exposure_allowed = portfolio.get_max_exposure_allowed()
+        print(f'[IN PROGRESS]:Currently on ticker:{ticker}')
+
+        # holdings is a dict where key = some ticker and value = another dictionary
+        # The value dictionary will hold the total invested amount and total number of
+        # shares. ---> {ticker:'GOOG',info:{shares:55,amount:$25,000}}
+        if ticker in holdings:
+            info:dict = holdings.get(ticker)
+            shares,amount = info.get("shares"),info.get("amount")
+            print(f'Ticker:{ticker}, Shares:{shares}, Total amount: {amount}, Amount Per Share: {amount/shares}')
+            port_perc:float = (amount/total_funds)*100
+
+            if(port_perc >= exposure_allowed):
+                print(f'The percentage of shares in your portfolio ({port_perc}) is greater than your max allowed exposure ({portfolio.get_max_exposure_allowed()})')
+            
+            else:
                 print(f"TICKER:{ticker} is {port_perc}% of the portfolio")
+                if(type == "ema"):
+                    stock_df = pd.DataFrame(ticker.get_info())
+                    stock_df[f'{window}-day ewma'] = stock_df["close"].ewm(span=window,adjust=False).mean()
+
+                    for _,row in stock_df.iterrows():
+
+                        price = row['close']
+                        mov_avg = row[f'{window}-day ewma']
+
+
+                else:
+                    stock_df = pd.DataFrame(ticker.get_info())
+                    stock_df[f'{window}-day ma'] = stock_df["close"].rolling(window=window).mean()
+
+                    for _,row in stock_df.iterrows():
+                        
+                        price = row['close']
+                        mov_avg = row[f'{window}-day ma']
+            
+        else:
+            print(f'TICKER:{ticker} is {0}% of your portfolio. Calculating percentages....')
+            if(type == "ema"):
 
                 
+                stock_df = pd.DataFrame(ticker.get_info())
+                stock_df[f'{window}-day ewma'] = stock_df["close"].ewm(span=window,adjust=False).mean()
+                buySellDict:dict = {}
+                curr_price_total = 0.0
+                curr_exposure:float = 0.0
+                shares = 0
+                # The derivative or rate of change between the stock
+                # and its moving average.
+                prev_roc = 0.0
+
+                for _,row in stock_df.iterrows():
+
+                    price = row['close']
+                    mov_avg = row[f'{window}-day ewma']
+                    curr_roc:float = abs(((price - mov_avg)/mov_avg)*100)
+
+                    if price > mov_avg:
+                        # We must calculate a threshold on what we want to constitute
+                        # a buy into the long term and the short term.
+                        if curr_exposure < exposure_allowed:
+
+                            if(curr_roc < prev_roc):
+                                # If triggered then we are contracting towards the mean (simple mean-reversion)
+                                # We will go ahead and shedd shares for profit
+
+                                # FIGURE OUT HOW MUCH WE WANT TO ACTUALLY SHED FOR PROFIT TAKING
+                                shares_to_sell = floor((curr_price_total*curr_exposure)/(shares/curr_price_total))
+                                if shares > 0 and shares - shares_to_sell >=0:
+                                    shares -= shares_to_sell
+                                else:
+                                # BUYING IN (FIGURE OUT THRESH HOLD FOR MAKING PURCHASE)
+                                    shares += 1
+                                    curr_price_total += price
+                                    curr_funds -= curr_price_total
+                                    curr_exposure = (shares/total_funds)*100
+                                    buySellDict[row['datetime']] = {"action":"buy","shares":shares - (shares-1)}
+                                    
+
+                        elif curr_exposure > exposure_allowed:
+                            shares_to_sell = floor((curr_price_total*curr_exposure)/(shares/curr_price_total))
+                            if(curr_roc < prev_roc):
+                                # PROFIT TAKE
+                                if shares > 0 and shares - shares_to_sell >=0:
+                                    shares -= shares_to_sell
+
+
+
+
+                    elif price < mov_avg:
+                        # We must calculate a threshold on what we want to constitute
+                        # a sell into the long term and the short term.
+                        if shares > 0:
+                            # Logic to determine how much to sell
+                            # This triggers more selling
+                            if curr_roc > prev_roc:
+                                pass
+                            # 
+
+                            # This triggers a look into buying because of contraction
+                            # within the derivative (i.e. the rate of change is slowing
+                            # which signals a consolidation or reversation)
+                            elif curr_roc < prev_roc:
+                                # Look into logic for constituting a buy
+                                pass
+
+                
+                
             else:
-                print("")
+                import pandas as pd
+                stock_df = pd.DataFrame(ticker.get_info())
+                stock_df[f'{window}-day ma'] = stock_df["close"].rolling(window=window).mean()
 
-    def execute_pairs_trading_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory]):
-        pass
-
-    def execute_scalping_strategy(self,portfolio:Portfolio, potential_stocks:Set[PriceHistory]):
-        pass
+                
